2fdf95b4f7f166a6b5b55deb916eeef59ffd3d0f
Vini Fraga <vinifragam@gmail.com>
2023-10-23 17:06:17 -0300

    Projeto de e-commerce - Criando estrutura do app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,8 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    fontFamily: {
+      sans: 'var(--font-inter)',
     },
   },
   plugins: [],
