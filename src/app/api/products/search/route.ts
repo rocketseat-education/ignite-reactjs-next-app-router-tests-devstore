5abca4d16b5e9658e906082d8a26b687e8939da5
Vini Fraga <vinifragam@gmail.com>
2023-10-24 16:12:12 -0300

    Projeto de e-commerce - API: Busca de produtos

---
+++ src/app/api/products/search/route.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod'
+import type { NextRequest } from 'next/server'
+
+import data from '../data.json'
+
+export async function GET(request: NextRequest) {
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+
+  const { searchParams } = request.nextUrl
+
+  const query = z.string().parse(searchParams.get('q'))
+
+  const products = data.products.filter((product) => {
+    return product.title.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+  })
+
+  return Response.json(products)
+}
