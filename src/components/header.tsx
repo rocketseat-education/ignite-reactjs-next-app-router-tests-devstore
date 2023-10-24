413a069326838adc87d9e82cc14c620603a28af4
Vini Fraga <vinifragam@gmail.com>
2023-10-24 17:34:47 -0300

    Projeto de e-commerce - Formulário de busca

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,8 @@
 import Link from 'next/link'
-import { Search } from 'lucide-react'
 import Image from 'next/image'
+
 import { CartWidget } from './cart-widget'
+import { SearchForm } from './search-form'
 
 export function Header() {
   return (
@@ -11,14 +12,7 @@ export function Header() {
           devstore
         </Link>
 
-        <form className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
-          <Search className="w-5 h-5 text-zinc-500" />
-
-          <input
-            placeholder="Buscar produtos..."
-            className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
-          />
-        </form>
+        <SearchForm />
       </div>
 
       <div className="flex items-center gap-4">
---
+++ src/components/search-form.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { Search } from 'lucide-react'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { FormEvent } from 'react'
+
+export function SearchForm() {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+
+  const query = searchParams.get('q')
+
+  function handleSearch(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+    const data = Object.fromEntries(formData)
+
+    const query = data.q
+
+    if (!query) {
+      return null
+    }
+
+    router.push(`/search?q=${query}`)
+  }
+
+  return (
+    <form
+      onSubmit={handleSearch}
+      className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700"
+    >
+      <Search className="w-5 h-5 text-zinc-500" />
+
+      <input
+        name="q"
+        defaultValue={query ?? ''}
+        placeholder="Buscar produtos..."
+        className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
+        required
+      />
+    </form>
+  )
+}
