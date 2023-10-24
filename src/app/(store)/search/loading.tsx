abfc6daf77dc41d78dc1faad542767d5fd5b4682
Vini Fraga <vinifragam@gmail.com>
2023-10-24 17:55:28 -0300

    Projeto de e-commerce - Loading da busca

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,3 +1,27 @@
+'use client'
+
+import { Skeleton } from '@/components/skeleton'
+import { useSearchParams } from 'next/navigation'
+
 export default function SearchLoading() {
-  return <p>Carregando busca...</p>
+  const searchParams = useSearchParams()
+
+  const query = searchParams.get('q')
+
+  return (
+    <div className="flex flex-col gap-4">
+      <p className="text-sm">
+        Resultados para: <span className="font-semibold">{query}</span>
+      </p>
+
+      <div className="grid grid-cols-3 gap-6">
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+      </div>
+    </div>
+  )
 }
