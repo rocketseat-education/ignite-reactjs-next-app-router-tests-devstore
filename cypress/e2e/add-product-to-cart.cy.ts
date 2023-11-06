a46dfbb4c4fb09c4084802449011f89c9ecf59dd
Vini Fraga <vinifragam@gmail.com>
2023-11-06 17:48:15 -0300

    Projeto de e-commerce - Testes do carrinho

---
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -0,0 +1,38 @@
+describe('add product to cart', () => {
+  it('should be able to navigate to the product page and add it to the cart', () => {
+    cy.visit('http://localhost:3000')
+
+    cy.get('a[href^="/product"]').first().click()
+
+    cy.location('pathname').should('include', '/product')
+    cy.contains('Adicionar ao carrinho').click()
+
+    cy.contains('Cart (1)').should('exist')
+  })
+
+  it('should not count duplicated products on cart', () => {
+    cy.visit('http://localhost:3000')
+
+    cy.get('a[href^="/product"]').first().click()
+
+    cy.location('pathname').should('include', '/product')
+    cy.contains('Adicionar ao carrinho').click()
+    cy.contains('Adicionar ao carrinho').click()
+
+    cy.contains('Cart (1)').should('exist')
+  })
+
+  it('should be able to search for a product and add it to the cart', () => {
+    cy.visit('http://localhost:3000')
+
+    cy.get('input[name=q]').type('moletom').parent('form').submit()
+
+    cy.get('a[href^="/product"]').first().click()
+
+    cy.location('pathname').should('include', '/product')
+
+    cy.contains('Adicionar ao carrinho').click()
+
+    cy.contains('Cart (1)').should('exist')
+  })
+})
