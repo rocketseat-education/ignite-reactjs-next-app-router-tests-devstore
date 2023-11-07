687ef5f5f1010084f8fc81f2d78798e807b95dab
Vini Fraga <vinifragam@gmail.com>
2023-11-07 11:40:57 -0300

    Projeto de e-commerce - Comandos personalizados

--- cypress/e2e/add-product-to-cart.cy.ts
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -23,7 +23,7 @@ describe('add product to cart', () => {
   })
 
   it('should be able to search for a product and add it to the cart', () => {
-    cy.get('input[name=q]').type('moletom').parent('form').submit()
+    cy.searchByQuery('moletom')
 
     cy.get('a[href^="/product"]').first().click()
 
--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -1,8 +1,6 @@
 describe('search products', () => {
   it('should be able to search for products', () => {
-    cy.visit('/')
-
-    cy.get('input[name=q]').type('moletom').parent('form').submit()
+    cy.searchByQuery('moletom')
 
     cy.location('pathname').should('include', '/search')
     cy.location('search').should('include', 'q=moletom')
