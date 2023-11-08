d5ca8a5828cb80656e30ffefc4c162b69ef6b85d
Vinícius Fraga Modesto <vinifragam@gmail.com>
2023-11-08 16:41:04 -0300

    Projeto de e-commerce - Ajustes no deploy da aplicação

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,18 +1,14 @@
-'use client'
+import { Suspense } from 'react'
 
 import { Skeleton } from '@/components/skeleton'
-import { useSearchParams } from 'next/navigation'
+import { CurrentSearch } from './current-search'
 
 export default function SearchLoading() {
-  const searchParams = useSearchParams()
-
-  const query = searchParams.get('q')
-
   return (
     <div className="flex flex-col gap-4">
-      <p className="text-sm">
-        Resultados para: <span className="font-semibold">{query}</span>
-      </p>
+      <Suspense fallback={null}>
+        <CurrentSearch />
+      </Suspense>
 
       <div className="grid grid-cols-3 gap-6">
         <Skeleton className="h-[400px]" />
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { Suspense } from 'react'
 
 import { CartWidget } from './cart-widget'
 import { SearchForm } from './search-form'
@@ -12,7 +13,9 @@ export function Header() {
           devstore
         </Link>
 
-        <SearchForm />
+        <Suspense fallback={null}>
+          <SearchForm />
+        </Suspense>
       </div>
 
       <div className="flex items-center gap-4">
